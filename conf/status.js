6d356af7d2f51d5b8a43b828c9e4d7c9039d28b8
唯人 <yuito_it@outlook.jp>
2023-07-31 09:25:39 +0900

    Log system is complated!!

--- conf/status.js
+++ conf/status.js
@@ -5,21 +5,28 @@ const deta = Deta();
 const db = deta.Base('log-conf');
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
+    console.log("GET!!");
     try {
-        var data = db.get("status");
+        const data = await db.get("status");
+        console.log(data);
         res.status(200).json(data);
     }
     catch (err) {
         res.status(500).json(err);
+        console.log(err);
         throw err;
     }
 });
 router.post('/', (req, res, next) => {
-    try{
-        db.put(req.body,"status");
+    console.log(req.body);
+    try {
+        db.put(req.body, "status");
+        console.log("putting!!");
+        console.log(req.body);
+        res.status(200).json(req.body);
     }
-    catch(err){
+    catch (err) {
         res.status(500).json(err);
         throw err;
     }
--- index.js
+++ index.js
@@ -4,8 +4,18 @@ const db = deta.Base('log-conf');
 
 const port = process.env.PORT || 8080;
 
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+
+app.use(bodyParser.json());
+app.use(express.json());
+
 const status = require('./conf/status.js');
 app.use('/v1/conf/status', status);
+
+/*
+app.use(express.urlencoded({
+    extended: true
+}));*/
 app.listen(port, () => console.log('listening on port 3000!'));
